fd5a5e7c86327fd2f62b990512014a592002fbeb
shubham kumar <clboy768@gmail.com>
2023-05-09 04:23:30 +0530

    added logout and redirect functionality

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,16 +1,19 @@
 import { Link, } from 'react-router-dom';
+import {useHistory} from 'react-router-dom'
 
 import classes from './MainNavigation.module.css';
 import { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 
 const MainNavigation = () => {
+  const history=useHistory()
 
   const authCtx=useContext(AuthContext)
   const isLoggedIn=authCtx.isLoggedIn
 
   const handleLogout=()=>{
     authCtx.logout()
+    history.replace('/auth')
   }
 
   return (
